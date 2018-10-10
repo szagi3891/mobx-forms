9d095758ee21832bef27258daa72b77ca3e40fea
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2018-10-10 07:30:28 +0200

    małe zmiany

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -20,33 +20,60 @@ const validateDay = validateRange(1, 31, 'Niepoprawny dzień');
 const validateMonth = validateRange(1, 12, 'Niepoprawny miesiąc');
 const validateYear = validateRange(1970, 2050, 'Niepoprawny rok');
 
+interface FromToType {
+    from: number,
+    to: number,
+}
+
+const acceptRange = (maxDelta: number) => (value: FromToType): FromToType | Error => {
+    const delta = Math.abs(value.to - value.from);
+    return delta > maxDelta ? new Error('Zbyt dua odległośc pomiędzy oboma liczbami') : value;
+}
+
+const field1 = FormValue
+    .init('')
+    .map(validateNotEmpty)
+    .map(convertToNumber)
+    .map(validateDay);
+
+const field2 = FormValue
+    .init('')
+    .map(validateNotEmpty)
+    .map(convertToNumber)
+    .map(validateMonth);
+
+const field3 = FormValue
+    .init('')
+    .map(validateNotEmpty)
+    .map(convertToNumber)
+    .map(validateYear);
+
+const date1 = FormGroup.init({
+    day: field1,
+    month: field2,
+    year: field3
+});
+
+const field4 = FormValue.init('')
+    .map(validateNotEmpty)
+    .map(convertToNumber);
+
+const field5 = FormValue.init('')
+    .map(validateNotEmpty)
+    .map(convertToNumber);
+
+const range = FormGroup.
+    init({
+        from: field4,
+        to: field5
+    })
+    .map(acceptRange(10))
+    .map((value): string | Error => `${value.from}-${value.to}`)
+;
 
 const formState = FormGroup.init({
-    data: FormGroup.init({
-        day: FormValue.init('')
-            .map(validateNotEmpty)
-            .map(convertToNumber)
-            .map(validateDay),
-        month: FormValue.init('')
-            .map(validateNotEmpty)
-            .map(convertToNumber)
-            .map(validateMonth),
-        year: FormValue.init('')
-            .map(validateNotEmpty)
-            .map(convertToNumber)
-            .map(validateYear)
-    }),
-    range: FormGroup.init({
-        from: FormValue.init('')
-            .map(validateNotEmpty)
-            .map(convertToNumber),
-        to: FormValue.init('')
-            .map(validateNotEmpty)
-            .map(convertToNumber)
-    })
-        .map((value): string | Error => {
-            return 'dasdsa';
-        })
+    data: date1,
+    range: range
 })
 
 @observer
